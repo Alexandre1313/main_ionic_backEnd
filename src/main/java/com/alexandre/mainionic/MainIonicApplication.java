8dfdf3b1c6163698fd70356257ce2e5c468f6d5d
Alexandre1313 <alexandre.13a@gmail.com>
2022-04-02 15:05:08 -0300

    starter project

---
+++ src/main/java/com/alexandre/mainionic/MainIonicApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.mainionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandre/mainionic/MainIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.mainionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
