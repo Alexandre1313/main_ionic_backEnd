a4e931f265c78210f951e38fae8e789499ea20e4
Alexandre1313 <alexandre.13a@gmail.com>
2022-04-02 23:53:43 -0300

    get byid implements in category

--- src/main/java/com/alexandre/mainionic/resources/CategoryResources.java
+++ src/main/java/com/alexandre/mainionic/resources/CategoryResources.java
@@ -1,10 +1,25 @@
 package com.alexandre.mainionic.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alexandre.mainionic.domain.Category;
+import com.alexandre.mainionic.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoryResources {
-
+	
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Category> findById(@PathVariable Integer id) {
+		Category obj = service.searchById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
---
+++ src/main/java/com/alexandre/mainionic/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.alexandre.mainionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandre.mainionic.domain.Category;
+import com.alexandre.mainionic.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repo;
+	
+	public Category searchById(Integer id) {
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
